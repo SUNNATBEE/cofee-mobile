26711975ac6e618f2d5d5bc2c9b44bea4f55f04a
sunnatbee <sunnatbekyusupov@icloud.com>
2022-08-21 12:53:30 +0500

    Full version

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ var ElOpenBtn = document.querySelector(".js-open");
 ElOpenBtn.addEventListener("click" , function(){
     ElOpenBtn.closest(".site-header").classList.toggle("sitenav-open");
 })
+
+// Start Modal Window
+
+let formBtn = document.querySelector(".accardion-part__btn")
+let modal = document.querySelector(".modal")
+let closeModalBtn = document.querySelector(".modal-part__desktop-link")
+let ElbodyOwerflow = document.querySelector("body")
+
+formBtn.addEventListener("click", ()=>{
+    modal.classList.add("modal-on")
+    ElbodyOwerflow.classList.add("hidden");
+})
+
+closeModalBtn.addEventListener("click", ()=> {
+    modal.classList.remove("modal-on")
+    ElbodyOwerflow.classList.removegi("hidden");
+})
+// End of Modal Window
